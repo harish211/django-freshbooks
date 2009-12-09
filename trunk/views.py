5413264bcc6ef46354a3f70a9cd4e3f0b8950662
justhamade <justhamade@3fb2f218-e15a-11de-a6a9-d5aeed48c2a6>
2009-12-09 02:42:49 +0000

    change api so model inherit from it

--- trunk/views.py
+++ trunk/views.py
@@ -6,10 +6,12 @@ def client_create(request):
     if request.method == 'POST': # If the form has been submitted...
         form = ClientForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
+            obj = form.save()
             return HttpResponseRedirect('/freshbooks/client/added') # Redirect after POST
-    else:
-        form = ClientForm() # An unbound form
+    
+    form = ClientForm() # An unbound form
 
     return render_to_response('form.html', { 'form': form, })
+
 def client_added(request):
     return render_to_response('added.html', )
