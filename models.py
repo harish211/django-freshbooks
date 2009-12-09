84b85ccdc617317c1e9c500affbdef9230fbcae2
justhamade <justhamade@3fb2f218-e15a-11de-a6a9-d5aeed48c2a6>
2009-12-09 05:25:35 +0000

    Add more objects mapped to the api

--- models.py
+++ models.py
@@ -3,14 +3,29 @@ from django_freshbooks import api
 from django_freshbooks.settings import *
 from datetime import date
 
-STATUS_CHOICE = (
+STATUS_CHOICES = (
                  ('sent','sent'),
                  ('viewed','viewed'),
                  ('paid','paid'),
                  ('draft','draft'),
                  )
+FREQ_CHOICES = (
+                 ('weekly','weekly'),
+                 ('2 weeks','2 weeks'),
+                 ('4 weeks','4 weeks'),
+                 ('monthly','monthly'),
+                 ('3 months','3 months'),
+                 ('6 months','6 months'),
+                 ('yearly','yearly'),
+                 ('2 years','2 years'),
+                 )
 
 class Line(api.BaseObject):
+    TYPE_MAPPINGS = {'invoice_id' : 'int', 'client_id' : 'int',
+        'po_number' : 'int', 'discount' : 'float', 'amount' : 'float',
+        'date' : 'datetime', 'amount_outstanding' : 'float', 
+        'paid' : 'float'}
+    
     name = models.CharField(max_length=255,blank=True)
     description = models.CharField(max_length=255,blank=True)
     unit_cost = models.DecimalField(max_digits=2,decimal_places=2,blank=True)
@@ -20,23 +35,93 @@ class Line(api.BaseObject):
     tax1_percent = models.DecimalField(max_digits=2,decimal_places=2,blank=True)
     tax2_percent = models.DecimalField(max_digits=2,decimal_places=2,blank=True)
    
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
-        line = api.Line()
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("line.create", {None:self})
+
+
+class Category(api.BaseObject):
+    object_name = 'category'
+    name = models.CharField(max_length=255)
+    
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("category.create", {None:self}) 
+            
+class Task(api.BaseObject):
+    object_name = 'task'
+    TYPE_MAPPINGS = {'rate': 'float', }
+    
+    name = models.CharField(max_length=255)
+    billable = models.BooleanField()
+    rate = models.DecimalField(max_digits=10,decimal_places=2)
+    description = models.CharField(max_length=255)
+    
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("task.create", {None:self}) 
+            
+            
+class Payment(api.BaseObject):
+    object_name = 'payment'
+    TYPE_MAPPINGS = {'client_id': 'int', 'invoice_id': 'int'}
+    
+    client_id = models.IntegerField()
+    invoice_id = models.IntegerField()
+    date = models.DateField()
+    amount = models.DeciamlField(max_digits=10,decimal_places=2)
+    type = models.CharField(max_length=255)
+    notes = models.CharField(max_length=255)
+    
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("payment.create", {None:self})
         
-        for key,value in self.cleaned_data.items():
-            setattr(line, key, value)
-        try:
-            response = api.call_api("line.create", {None:client})
-        except Exception as msg:
-            raise models.ValidationError(msg)
-        
+class Recurring(api.BaseObject):
+    object_name = 'recurring'
+    TYPE_MAPPINGS = {'recurring_id' : 'int', 'client_id' : 'int',
+        'po_number' : 'int', 'discount' : 'float', 'amount' : 'float',
+        'date' : 'datetime', 'amount_outstanding' : 'float', 
+        'paid' : 'float'}
+    client_id = models.IntegerField()
+    date = models.DateField(default=date.today(),blank=True)
+    po_number = models.CharField(max_length=255,blank=True)
+    discount = models.DecimalField(blank=True,max_digits=2,decimal_places=2)
+    occurrences = models.IntegerField(help_text='0 = infinite',default=1)
+    frequency = models.CharField(max_length=50, choices=FREQ_CHOICES)
+    send_email = models.BooleanField()
+    send_snail_mail = models.BooleanField()
+    notes = models.CharField(max_length=255,blank=True,)
+    terms = models.CharField(max_length=255,blank=True)
+    return_uri = models.CharField(max_length=255,blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    organization = models.CharField(max_length=100)
+    p_street1 = models.CharField(max_length=255,blank=True)
+    p_street2 = models.CharField(max_length=255,blank=True)
+    p_city = models.CharField(max_length=255,blank=True)
+    p_state = models.CharField(max_length=255,blank=True)
+    p_country = models.CharField(max_length=255,blank=True)
+    p_code = models.CharField(max_length=255,blank=True)
+
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("invoice.create", {None:self})
         
 class Invoice(api.BaseObject):
+    object_name = 'invoice'
+    TYPE_MAPPINGS = {'invoice_id' : 'int', 'client_id' : 'int',
+        'po_number' : 'int', 'discount' : 'float', 'amount' : 'float',
+        'date' : 'datetime', 'amount_outstanding' : 'float', 
+        'paid' : 'float'}
     client_id = models.IntegerField()
     number = models.CharField(max_length=255,blank=True)
-    status = models.CharField(max_length=50,choices=STATUS_CHOICE,default='draft')
+    status = models.CharField(max_length=50,choices=STATUS_CHOICES,default='draft')
     date = models.DateField(default=date.today(),blank=True)
     po_number = models.CharField(max_length=255,blank=True)
     discount = models.DecimalField(blank=True,max_digits=2,decimal_places=2)
@@ -53,23 +138,17 @@ class Invoice(api.BaseObject):
     p_country = models.CharField(max_length=255,blank=True)
     p_code = models.CharField(max_length=255,blank=True)
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
-        invoice = api.Invoice()
-        
-        for key,value in self.cleaned_data.items():
-            setattr(client, key, value)
-        try:
-            response = api.call_api("invoice.create", {None:client})
-        except Exception as msg:
-            raise models.ValidationError(msg)
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("invoice.create", {None:self})
         
     
         
 class Client(api.BaseObject):
     TYPE_MAPPINGS = {'client_id' : 'int'}
     object_name = 'client'
+    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     organization = models.CharField(max_length=100)
@@ -95,25 +174,22 @@ class Client(api.BaseObject):
     s_code = models.CharField(max_length=255,blank=True)
 
     def save(self):
-        api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
-        response = api.call_api("client.create", {None:self})
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("client.create", {None:self})
 
 
 class Item(api.BaseObject):
-    '''
-    The Item object
-    '''
-
     object_name = 'item'
-    TYPE_MAPPINGS = {'item_id' : 'int', 'unit_cost' : 'float',
-        'quantity' : 'int', 'inventory' : 'int'}
-
-    def __init__(self):
-        '''
-        The constructor is where we initially create the
-        attributes for this class
-        '''
-        for att in ('item_id', 'name', 'description', 'unit_cost',
-        'quantity', 'inventory'):
-            setattr(self, att, None)
+    TYPE_MAPPINGS = {'item_id' : 'int', 'unit_cost' : 'float','quantity' : 'int', 'inventory' : 'int'}
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    unit_cost = models.DecimalField(max_digits=10,decimal_places=2)
+    quantity = models.IntegerField()
+    inventory = models.IntegerField(blank=True)
+ 
+    def save(self):
+        if not self.id:
+            api.setup(FRESHBOOKS_URL,FRESHBOOKS_TOKEN);
+            response = api.call_api("item.create", {None:self})
 
