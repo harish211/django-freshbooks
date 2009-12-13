2c9c032a1939aa15ca4aafc860a9fc14b5d1eca2
justhamade <justhamade@3fb2f218-e15a-11de-a6a9-d5aeed48c2a6>
2009-12-13 18:24:27 +0000

    Remove admin Add generic urls Modify view to use getattr and dynamically create the form needed

--- trunk/urls.py
+++ trunk/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import *
 from django_freshbooks.views import *
 urlpatterns = patterns('',
-    (r'^client/create/', client_create),
-    (r'^client/added/', client_added),
-    (r'^category/create/', category_create),
-    (r'^category/added/', category_added),
+    url(r'^create/(?P<form_type>\w+)/$', form_create, name='form_create'),
+    url(r'^added/(?P<form_type>\w+)/$', form_added, name='form_added'),
 )
