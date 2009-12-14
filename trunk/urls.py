cc77a7eccafd95f1a0c2328aacebc31a0cce93fe
justhamade <justhamade@3fb2f218-e15a-11de-a6a9-d5aeed48c2a6>
2009-12-14 00:52:30 +0000

    Add inline formset for line items

--- trunk/urls.py
+++ trunk/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 from django_freshbooks.views import *
 urlpatterns = patterns('',
     url(r'^create/(?P<form_type>\w+)/$', form_create, name='form_create'),
+    url(r'^create_inline/(?P<form_type>\w+)/$', inline_form_create, name='inline_form_create'),
     url(r'^added/(?P<form_type>\w+)/$', form_added, name='form_added'),
 )
--- trunk/views.py
+++ trunk/views.py
@@ -4,6 +4,8 @@ from django_freshbooks.refreshbooks import api
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.forms.formsets import formset_factory
+from django.http import Http404
 
 def auth_freshbooks(type='token'):
     if type == 'oauth':
@@ -24,9 +26,12 @@ def auth_freshbooks(type='token'):
 
 def form_create(request,form_type):
     '''
-    form_type must be a simple type with no relationships
-    Category, Client, Item, Staff, Task
+    form_type must be a simple type with no line items
+    Category, Client, Expense, Item, Payment, Project, Staff, Task, Time Entry
     '''
+    if form_type not in ('category','client','expense','item','payment','project','staff','task','timeentry'):
+        raise Http404
+    
     form_class = form_type.capitalize() + 'Form'
     if request.method == 'POST': # If the form has been submitted...
         form = getattr(forms,form_class)(request.POST) # A form bound to the POST data
@@ -40,6 +45,30 @@ def form_create(request,form_type):
     form = getattr(forms,form_class)()# An unbound form
 
     return render_to_response('form.html', { 'form': form, })
+
+def inline_form_create(request,form_type):
+    '''
+    form_type must be a simple type with no relationships
+    Category, Client, Item, Staff, Task
+    '''
+    if form_type not in ('estimate','invoice','recurring'):
+        raise Http404
+    form_class = form_type.capitalize() + 'Form'
+    LineFormSet = formset_factory(forms.LineForm, extra=2)
+    if request.method == 'POST': # If the form has been submitted...
+        form = getattr(forms,form_class)(request.POST) # A form bound to the POST data
+        formset = LineFormSet(request.POST)
+        if form.is_valid(): # All validation rules pass
+            fb = auth_freshbooks()
+            fb_kwargs = {'category': form.cleaned_data}
+            func_type = getattr(fb, form_type)
+            func_type.create(**fb_kwargs)
+            return HttpResponseRedirect(reverse('form_added',kwargs={'form_type':form_type})) # Redirect after POST
+    
+    form = getattr(forms,form_class)()# An unbound form
+    formset = LineFormSet()
+
+    return render_to_response('form.html', { 'form': form, 'formset':formset})
 
 def form_added(request,form_type):
     return render_to_response('added.html', {'type':form_type})
